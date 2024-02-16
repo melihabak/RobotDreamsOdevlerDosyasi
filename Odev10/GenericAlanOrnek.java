6a4cbb23812879ae562fe57c7a976d40d5c15c82
melihabak <melihfurkanabak@gmail.com>
2024-02-16 12:19:33 +0300

    Odevin ilk kismi tamamlandi

---
+++ Odev10/GenericAlanOrnek.java
@@ -0,0 +1,24 @@
+public class GenericAlanOrnek {
+
+    public static void main(String[] args) {
+
+        TestGeneric<Integer> integerTestGeneric = new TestGeneric<Integer>(45);
+        System.out.println("Integer Ornek Sonucu : "+integerTestGeneric.getObject());
+
+        TestGeneric<String> sObj = new TestGeneric<>("Test Otomasyon Kursuna Hosgeldiniz");
+        System.out.println("String Ornek Sonucu : "+sObj.getObject());
+
+    }
+    static class TestGeneric <T> {
+        T obj; //T tipinde bir nesne bildirildi
+
+        // Constructur olusturuldu
+        public TestGeneric(T obj) {
+            this.obj = obj;
+        }
+
+        public T getObject() {
+            return this.obj;
+        }
+    }
+}
