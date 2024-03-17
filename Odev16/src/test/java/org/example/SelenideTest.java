fcf59012644f6e313884a571785309d75b529eee
melihabak <melihfurkanabak@gmail.com>
2024-03-17 14:41:31 +0300

    Odev16 tamamlandi

--- Odev16/src/test/java/org/example/SelenideTest.java
+++ Odev16/src/test/java/org/example/SelenideTest.java
@@ -4,11 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class Demo {
-
-    public static void main(String[] args) throws InterruptedException {
-
+public class SelenideTest {
+    public static String denemeTest() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\melih.abak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://demoqa.com/elements");
@@ -18,5 +17,13 @@ public class Demo {
         String okunanMesaj = driver.findElement(By.cssSelector("#dynamicClickMessage")).getText();
         System.out.println(okunanMesaj);
         driver.quit();
+        return okunanMesaj;
+    }
+    @Test
+    public void okunanMesajTestiBir() throws InterruptedException {
+
+        String okunmusDeger = denemeTest();
+        String beklenenMesaj = "You have done a dynamic click";
+        Assertions.assertThat(okunmusDeger).isEqualTo(beklenenMesaj);
     }
 }
---
+++ Odev16/src/test/java/org/example/SelenideTest2.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class SelenideTest2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\melih.abak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        //driver.get("https://demoqa.com/webtables");
+        Configuration.holdBrowserOpen = true;
+        Selenide.open("https://demoqa.com/webtables");
+        Thread.sleep(1000);
+        SelenideElement addButton = $("#addNewRecordButton");
+        addButton.click();
+        Thread.sleep(1000);
+        SelenideElement usernameInput = $("#firstName");
+        usernameInput.setValue("Melih");
+        Thread.sleep(1000);
+        SelenideElement lastnameInput = $("#lastName");
+        lastnameInput.setValue("Abak");
+        Thread.sleep(1000);
+        SelenideElement emailInput = $("#userEmail");
+        emailInput.setValue("sjdnsdnldsvmsm@gmail.com");
+        Thread.sleep(1000);
+        SelenideElement ageInput = $("#age");
+        ageInput.setValue("15");
+        Thread.sleep(1000);
+        SelenideElement salaryInput = $("#salary");
+        salaryInput.setValue("15");
+        Thread.sleep(1000);
+        SelenideElement departmentInput = $("#department");
+        departmentInput.setValue("ino");
+        Thread.sleep(1000);
+        SelenideElement submitButton = $("#submit");
+        submitButton.click();
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
