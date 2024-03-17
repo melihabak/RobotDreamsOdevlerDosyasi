57f9f484c2aa137286be749fc5da467ab37f4da1
melihabak <melihfurkanabak@gmail.com>
2024-03-17 11:36:10 +0300

    Odev15 birinci kisim tamamlandi

---
+++ Odev15/src/main/java/org/example/DemoXPathSelector.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoXPathSelector {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\melih.abak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/elements");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[5]/span[1]")).click();
+        driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/div[3]/button[1]")).click();
+        String okunanMesaj = driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/p[1]")).getText();
+        System.out.println(okunanMesaj);
+        driver.quit();
+
+    }
+}
