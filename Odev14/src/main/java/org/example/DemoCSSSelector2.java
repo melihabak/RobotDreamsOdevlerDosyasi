78ae01f5927765a287cdba95d8d1d27fd31faf6f
melihabak <melihfurkanabak@gmail.com>
2024-03-17 11:56:04 +0300

    odev 14 ikinci madde tamamlandi

---
+++ Odev14/src/main/java/org/example/DemoCSSSelector2.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoCSSSelector2 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\melih.abak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/webtables");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#addNewRecordButton")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#firstName")).sendKeys("Melih");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#lastName")).sendKeys("Abak");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#userEmail")).sendKeys("mfascasdcascas@gmail.com");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#age")).sendKeys("15");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#salary")).sendKeys("20");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#department")).sendKeys("ino");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#submit")).click();
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
