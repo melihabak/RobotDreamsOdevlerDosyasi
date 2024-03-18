2daaff44bd0db1d77eac881d1b6a4563e05f27f0
melihabak <melihfurkanabak@gmail.com>
2024-03-18 09:05:55 +0300

    Odev 11 tamamlandi

--- Odev11/Singleton.java
+++ Odev11/Singleton.java
@@ -1,5 +1,4 @@
 
-
     // Ensure a class only has one instance, and provide a global point of access to it.
 
     public class Singleton {
--- Odev11/SingletonClient.java
+++ Odev11/SingletonClient.java
@@ -1,5 +1,4 @@
 public class SingletonClient {
-
     public static void main(String[] args) {
         for (int i = 0; i<10; i++){
             Singleton s = Singleton.getInstance();
