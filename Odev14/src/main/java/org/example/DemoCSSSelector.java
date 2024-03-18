699e1bb24c79488411522b3cb8c4b25e58cc8467
melihabak <melihfurkanabak@gmail.com>
2024-03-18 08:55:58 +0300

    odev 14 her iki kisim da tamamlandi

--- Odev14/src/main/java/org/example/DemoCSSSelector.java
+++ Odev14/src/main/java/org/example/DemoCSSSelector.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoCSSSelector {
-
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\melih.abak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
