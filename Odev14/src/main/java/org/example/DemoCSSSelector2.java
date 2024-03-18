7fb4c50c2b09be5194edb2d0f43ce99ad28a68c4
melihabak <melihfurkanabak@gmail.com>
2024-03-18 09:19:00 +0300

    odev14 her iki kisim da tamamlandi

--- Odev14/src/main/java/org/example/DemoCSSSelector2.java
+++ Odev14/src/main/java/org/example/DemoCSSSelector2.java
@@ -3,6 +3,7 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DemoCSSSelector2 {
     public static void main(String[] args) throws InterruptedException {
 
