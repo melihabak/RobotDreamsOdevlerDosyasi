0f5d70c063441b727b01faf4b149793c16f5c6f1
melihabak <melihfurkanabak@gmail.com>
2024-03-18 09:17:37 +0300

    odev tamamlandi

--- Odev14/src/main/java/org/example/DemoCSSSelector.java
+++ Odev14/src/main/java/org/example/DemoCSSSelector.java
@@ -3,7 +3,6 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class DemoCSSSelector {
     public static void main(String[] args) throws InterruptedException {
 
--- Odev14/src/main/java/org/example/DemoCSSSelector2.java
+++ Odev14/src/main/java/org/example/DemoCSSSelector2.java
@@ -3,9 +3,7 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class DemoCSSSelector2 {
-
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\melih.abak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
