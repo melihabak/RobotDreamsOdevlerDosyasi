41229a836d08f90b862d030039ae14b2ad865687
melihabak <melihfurkanabak@gmail.com>
2024-03-18 09:02:24 +0300

    odev17 proje baslatildi

--- Odev17/src/main/java/org/example/Demo.java
+++ Odev17/src/main/java/org/example/Demo.java
@@ -2,9 +2,7 @@ package org.example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Demo {
-
     public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\melih.abak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
