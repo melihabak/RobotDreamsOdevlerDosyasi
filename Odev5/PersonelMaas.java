edac7afd68b8d5104653acf277737322d8635193
melihabak <melihfurkanabak@gmail.com>
2024-05-06 09:16:32 +0300

    Maas Hesaplama Sinifindaki Hata Giderildi

--- Odev5/PersonelMaas.java
+++ Odev5/PersonelMaas.java
@@ -4,14 +4,14 @@ public class PersonelMaas {
         int maas = 20000 ; //sabit maas
         Scanner input = new Scanner(System.in);
         int a;
-        System.out.print("Çalışılan gün sayısını giriniz : ");
+        System.out.print("Calisilan Gun Sayisini Giriniz : ");
         a = input.nextInt();
         if (a<=25){
-            System.out.println("Çalışan maaşı : "+maas + " Türk Lirası");
+            System.out.println("Calisan Maasi : "+maas + " Turk Lirasi");
         } else if (a<=31) {
-            System.out.println("Çalışan maaşı : "+(maas+(a-25)*1000)+ " Türk Lirası");
+            System.out.println("Calisan Maasi : "+(maas+(a-25)*1000)+ " Turk Lirasi");
         }else {
-            System.out.println("Geçersiz Çalışma Gün Sayısı Girişi Yaptınız");
+            System.out.println("Gecersiz Calisma Gun Sayisi Girisi Yaptiniz");
         }
     }
     public static void main(String args[]) {
